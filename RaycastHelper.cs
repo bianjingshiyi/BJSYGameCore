3fea4bd3d27b4ca45450495379e943401ac0e060
BJSY <bianjingshiyi@163.com>
2019-04-18 16:54:39 +0800

    新增RaycastHelper

---
+++ RaycastHelper.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+namespace BJSYGameCore
+{
+    public static class RaycastHelper
+    {
+        public static T raycast<T>(Ray ray, float maxDistance, int layerMask) where T : Component
+        {
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit, maxDistance, layerMask);
+            if (hit.collider != null)
+                return hit.collider.GetComponentInChildren<T>();
+            else
+                return default;
+        }
+    }
+}
