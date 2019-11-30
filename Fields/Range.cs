1f565b2a0e694773806f3942f3260cd036d756b8
BJSY <bianjingshiyi@hotmail.com>
2019-11-30 17:01:25 +0800

    新增Range

---
+++ Fields/Range.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BJSYGameCore
+{
+    [Serializable]
+    public class Range
+    {
+        public float min;
+        public float max;
+        public Range()
+        {
+            min = 0;
+            max = 0;
+        }
+        public Range(float min, float max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+    }
+}
