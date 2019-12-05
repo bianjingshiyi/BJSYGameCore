bde1a0093e57898f92fd97559046d09a5afe679a
BJSY <bianjingshiyi@hotmail.com>
2019-12-05 21:26:35 +0800

    新增Timer。

---
+++ Fields/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+namespace BJSYGameCore
+{
+    [Serializable]
+    public class Timer
+    {
+        public float startTime = -1;
+        public float duration = 0;
+        public bool isStarted
+        {
+            get { return startTime >= 0; }
+        }
+        public void reset()
+        {
+            startTime = -1;
+        }
+        public void start()
+        {
+            startTime = Time.time;
+        }
+        public void start(float now)
+        {
+            startTime = now;
+        }
+        public bool isExpired()
+        {
+            return Time.time - startTime >= duration;
+        }
+        public bool isExpired(float now)
+        {
+            return now - startTime >= duration;
+        }
+    }
+}
