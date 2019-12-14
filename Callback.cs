533c8001c97cbaf66b31ddd1feb0f9d463c95a34
BJSY <bianjingshiyi@hotmail.com>
2019-12-14 16:48:30 +0800

    新增Callback

---
+++ Callback.cs
@@ -0,0 +1,18 @@
+﻿namespace BJSYGameCore
+{
+    public class Callback
+    {
+        public delegate void Delegate(object[] args);
+        object[] args { get; }
+        Delegate callback { get; }
+        public Callback(object[] args, Delegate callback)
+        {
+            this.args = args;
+            this.callback = callback;
+        }
+        public void call()
+        {
+            callback?.Invoke(args);
+        }
+    }
+}
