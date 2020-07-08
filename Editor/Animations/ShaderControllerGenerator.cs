9d82e4b79387219848b50e63f1894fc04bbba18d
BJSY <bianjingshiyi@hotmail.com>
2020-07-08 14:19:57 +0800

    新增ShaderControllerGenerator定义

---
+++ Editor/Animations/ShaderControllerGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using System.CodeDom;
+
+namespace BJSYGameCore.Animations
+{
+    public class ShaderControllerGenerator
+    {
+        public CodeCompileUnit generateController(Shader shader, string Namespace)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
