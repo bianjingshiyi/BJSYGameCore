1fe45dc56847724a6870f4dd2db9c791916f1208
BJSY <bianjingshiyi@163.com>
2020-07-17 21:08:31 +0800

    尝试新增动画编辑器。

--- Editor/AnimNamePropDrawer.cs
+++ Editor/AnimNamePropDrawer.cs
@@ -2,16 +2,100 @@
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.Animations;
 
 namespace BJSYGameCore
 {
+    public static class ReflectionHelper
+    {
+        public static Assembly[] assemblies { get; } = loadAssemblies();
+        public static Assembly[] loadAssemblies()
+        {
+            return AppDomain.CurrentDomain.GetAssemblies();
+        }
+        public static Type[] types { get; } = loadTypes();
+        public static Type[] loadTypes()
+        {
+            return getTypes(assemblies);
+        }
+
+        private static Type[] getTypes(Assembly[] assemblies)
+        {
+            List<Type> list = new List<Type>();
+            foreach (var assembly in assemblies)
+            {
+                list.AddRange(assembly.GetTypes());
+            }
+            return list.ToArray();
+        }
+
+        public static Type getType(string typeName)
+        {
+            if (string.IsNullOrEmpty(typeName))
+                return null;
+            Type type = Type.GetType(typeName);
+            if (type != null)
+                return type;
+            foreach (Assembly assembly in assemblies)
+            {
+                type = assembly.GetType(typeName);
+                if (type != null)
+                    return type;
+            }
+            return type;
+        }
+        public static Type[] getSubclass(Assembly[] assemblies, Type baseType)
+        {
+            if (assemblies == null || assemblies.Length < 1)
+                assemblies = ReflectionHelper.assemblies;
+            return getTypes(assemblies).Where(t => t.BaseType == baseType || t.IsSubclassOf(baseType)).ToArray();
+        }
+    }
     [CustomPropertyDrawer(typeof(TypeStringAttribute))]
     class TypeStringPropDrawer : PropertyDrawer
     {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.String, label);
+        }
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.String)
+                EditorGUI.LabelField(position, "Field type must be string");
+            TypeStringAttribute att = attribute as TypeStringAttribute;
 
+            Rect labelPos = new Rect(position);
+            labelPos.width /= 2;
+            GUI.Label(labelPos, label);
+
+            Rect btnPos = new Rect(position);
+            btnPos.width /= 2;
+            btnPos.position += Vector2.right * btnPos.width;
+            Type currentType = ReflectionHelper.getType(property.stringValue);
+            if (GUI.Button(btnPos, new GUIContent(currentType != null ? currentType.Name : "None")))
+            {
+                GenericMenu menu = new GenericMenu();
+                menu.AddItem(new GUIContent("None"), currentType == null, () =>
+                {
+                    property.stringValue = string.Empty;
+                    property.serializedObject.ApplyModifiedProperties();
+                });
+                Type[] types = att.baseType == null ? ReflectionHelper.types : ReflectionHelper.getSubclass(att.assemblies, att.baseType);
+                foreach (var type in types)
+                {
+                    string typeName = type.FullName.Replace('.', '/').Replace('+', '/');
+                    menu.AddItem(new GUIContent(typeName), type == currentType, () =>
+                    {
+                        property.stringValue = type.FullName;
+                        property.serializedObject.ApplyModifiedProperties();
+                    });
+                }
+                menu.DropDown(btnPos);
+            }
+        }
     }
     [CustomPropertyDrawer(typeof(AnimNameAttribute))]
     class AnimNamePropDrawer : PropertyDrawer
@@ -23,7 +107,7 @@ namespace BJSYGameCore
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             if (property.propertyType != SerializedPropertyType.String)
-                EditorGUI.LabelField(position, "Type must be string");
+                EditorGUI.LabelField(position, "Field type must be string");
             if (property.serializedObject.targetObject is Component component)
             {
                 Animator animator = component.GetComponentInChildren<Animator>();
--- TypeStringAttribute.cs
+++ TypeStringAttribute.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using UnityEngine;
+using System.Reflection;
 namespace BJSYGameCore
 {
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
     public class TypeStringAttribute : PropertyAttribute
     {
+        public Assembly[] assemblies { get; } = null;
         public Type baseType { get; }
-        public TypeStringAttribute(Type baseType = null)
+        public TypeStringAttribute(Type baseType)
         {
             this.baseType = baseType;
+            this.assemblies = assemblies;
         }
     }
 }
