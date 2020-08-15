5514184bad5067f12d2dd3481d162e5ef279a098
BJSY <bianjingshiyi@163.com>
2020-08-15 23:49:36 +0800

    新增TaskExceptionHandler

---
+++ TaskExceptionHandler.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using System.Threading.Tasks;
+namespace BJSYGameCore
+{
+    public static class TaskExceptionHandler
+    {
+        static bool registered { get; set; } = false;
+        public static void register()
+        {
+            if (!registered)
+            {
+                TaskScheduler.UnobservedTaskException += (sender, obj) =>
+                {
+                    if (obj == null)
+                        return;
+                    if (obj.Exception.InnerExceptions != null && obj.Exception.InnerExceptions.Count > 1)
+                    {
+                        foreach (var exception in obj.Exception.InnerExceptions)
+                        {
+                            Debug.LogError(exception);
+                        }
+                    }
+                    else if (obj.Exception != null && obj.Exception.InnerException != null)
+                        Debug.LogError(obj.Exception.InnerException);
+                    else
+                        Debug.LogError(obj.Exception);
+                    obj.SetObserved();
+                };
+                registered = true;
+            }
+        }
+    }
+}
+#endif
