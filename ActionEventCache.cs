bead8a2b9f8919d0329d67e59e4e2a2ce35cd1ce
BJSY <bianjingshiyi@163.com>
2020-08-20 19:44:25 +0800

    将ActionEventCache移动到GameCore中。

---
+++ ActionEventCache.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+namespace BJSYGameCore
+{
+    public class ActionEventCache
+    {
+        List<CachedAction> cacheList { get; } = new List<CachedAction>();
+        public void cache<T1>(Action<T1> handler, T1 t1)
+        {
+            cacheList.Add(new CachedAction<T1>(handler, t1));
+        }
+        abstract class CachedAction
+        {
+            public abstract void handle();
+            public abstract string toString();
+            public override string ToString()
+            {
+                return toString();
+            }
+        }
+        class CachedAction<T1> : CachedAction
+        {
+            Action<T1> handler;
+            T1 t1;
+            public CachedAction(Action<T1> handler, T1 t1)
+            {
+                this.handler = handler;
+                this.t1 = t1;
+            }
+            public override void handle()
+            {
+                handler.Invoke(t1);
+            }
+
+            public override string toString()
+            {
+                return handler.Method.Name + "(" + t1 + ")";
+            }
+        }
+        public void poll()
+        {
+            foreach (var cachedEvent in cacheList)
+            {
+                try
+                {
+                    cachedEvent.handle();
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError("处理事件" + cachedEvent + "发生异常：" + e);
+                }
+            }
+            cacheList.Clear();
+        }
+        public void clear()
+        {
+            cacheList.Clear();
+        }
+    }
+}
