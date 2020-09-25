79f3ae713807f7386effe726315c93e3fcfff673
bianjingshiyi <bianjingshiyi@hotmail.com>
2020-09-25 20:06:23 +0800

    新增 KeyGeneration

--- LinqHelper.cs
+++ LinqHelper.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 namespace BJSYGameCore
 {
-    public static class LinqExtension
+    public static class LinqHelper
     {
         public static IEnumerable<T> skipUntil<T>(this IEnumerable<T> c, Func<T, bool> func)
         {
