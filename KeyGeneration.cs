f6d73ddb2a8555e5f496adde58e2b25925b6c5fa
bianjingshiyi <bianjingshiyi@hotmail.com>
2020-10-23 19:17:34 +0800

    key生成bug修复。

--- KeyGeneration.cs
+++ KeyGeneration.cs
@@ -13,14 +13,14 @@ namespace BJSYGameCore
         /// <param name="maxTime">生成key的最大次数，防止死循环。默认为36的Count次幂</param>
         /// <param name="existingKeys">已经存在的Key的集合，防止重复</param>
         /// <returns></returns>
-        public static string[] generateUniqueKeys(int count, int keyLength = -1, int maxTime = -1, ISet<string> existingKeys = null)
+        public static string[] generateUniqueKeys(int count, int keyLength = -1, double maxTime = -1, ISet<string> existingKeys = null)
         {
             if (count < 1)
                 throw new ArgumentException("生成字符串的数量不能小于1", nameof(count));
             if (keyLength == 0 || keyLength > 32)
                 throw new ArgumentException("Key的长度不能为0或者超过32", nameof(keyLength));
             if (maxTime < count)
-                maxTime = (int)Math.Pow(36, count);
+                maxTime = 36 * count;
             HashSet<string> keySet = new HashSet<string>();
             int time = 0;
             do
