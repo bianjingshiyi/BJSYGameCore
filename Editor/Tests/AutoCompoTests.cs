c49f13199b5507bcead0a5d748689ad67a79a172
hbn2020 <hbn233@163.com>
2020-11-11 15:37:30 +0800

    把测试补完

--- Editor/Tests/AutoCompoTests.cs
+++ Editor/Tests/AutoCompoTests.cs
@@ -51,6 +51,7 @@ namespace Tests
             Assert.AreEqual(gameObject.GetInstanceID(), (Class.CustomAttributes[0].Arguments[0].Value as CodePrimitiveExpression).Value);
             Assert.AreEqual(TypeAttributes.Public | TypeAttributes.Class, Class.TypeAttributes);
             Assert.AreEqual("Panel", Class.Name);
+
             CodeMemberField field = Class.Members[0] as CodeMemberField;
             Assert.AreEqual("SerializeField", field.CustomAttributes[0].Name);
             Assert.AreEqual(MemberAttributes.Private | MemberAttributes.Final, field.Attributes);
@@ -63,6 +64,32 @@ namespace Tests
             Assert.True(prop.HasGet);
             //...剩下的我懒得写了，自己对着下面注释掉的代码补完测试代码吧。
             //注意不要把不包括在内的background生成进去了哦。
+            CodeMemberField imageField = Class.Members[2] as CodeMemberField;
+            Assert.AreEqual("SerializeField", imageField.CustomAttributes[0].Name);
+            Assert.AreEqual(MemberAttributes.Private | MemberAttributes.Final, imageField.Attributes);
+            Assert.AreEqual("Image", imageField.Type.BaseType);
+            Assert.AreEqual("_returnImage", imageField.Name);
+            CodeMemberProperty imageProp = Class.Members[3] as CodeMemberProperty;
+            Assert.AreEqual(MemberAttributes.Public | MemberAttributes.Final, imageProp.Attributes);
+            Assert.AreEqual("Image", imageProp.Type.BaseType);
+            Assert.AreEqual("returnImage", imageProp.Name);
+            Assert.True(imageProp.HasGet);
+
+            CodeMemberField buttonField = Class.Members[4] as CodeMemberField;
+            Assert.AreEqual("SerializeField", buttonField.CustomAttributes[0].Name);
+            Assert.AreEqual(MemberAttributes.Private | MemberAttributes.Final, buttonField.Attributes);
+            Assert.AreEqual("Button", buttonField.Type.BaseType);
+            Assert.AreEqual("_returnButton", buttonField.Name);
+            CodeMemberProperty buttonProp = Class.Members[5] as CodeMemberProperty;
+            Assert.AreEqual(MemberAttributes.Public | MemberAttributes.Final, buttonProp.Attributes);
+            Assert.AreEqual("Button", buttonProp.Type.BaseType);
+            Assert.AreEqual("returnButton", buttonProp.Name);
+            Assert.True(buttonProp.HasGet);
+
+            CodeMemberMethod autoBindMethod = Class.Members[6] as CodeMemberMethod;
+            Assert.AreEqual(MemberAttributes.Public | MemberAttributes.Final, autoBindMethod.Attributes);
+            Assert.AreEqual("autoBind", autoBindMethod.Name);
+
         }
     }
 }
