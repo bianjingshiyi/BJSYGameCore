6656442a5fa8fced310e5ccfdee746b24a73bfaa
SiberiaYa <2041744819@qq.com>
2021-03-29 00:23:25 +0800

    搭建虚拟列表测试场景，写好初始化代码

---
+++ Tests/VirtualListTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BJSYGameCore.UI;
+using UnityEngine.UI;
+
+public struct TestDat
+{
+    public int data1;
+    public string data2;
+    public Vector2 data3;
+}
+
+
+public class VirtualListTest : MonoBehaviour
+{
+    VirtualList<UIObject> virtualList;
+    GridLayoutGroup glg;
+    public UIObject itemObj;
+    List<TestDat> testDatList = new List<TestDat>();
+ 
+    void initData()
+    {
+        for(int i = 0; i < 1000; i++)
+        {
+            testDatList.Add(new TestDat { data1 = i, data2 = (1000-i).ToString(),data3 = new Vector2(i,-i) }) ;
+        }
+    }
+
+    private void Awake()
+    {
+        initData();
+    }
+
+    private void Start()
+    {
+        glg = GetComponent<GridLayoutGroup>();
+        virtualList = new VirtualList<UIObject>(generateItem,glg);
+        virtualList.onDisPlayItem += setItemView;
+        initItemObj();
+    }
+
+    void initItemObj()
+    {
+        for(int i=0;i<testDatList.Count;i++)
+            virtualList.addItem();
+    }
+
+    UIObject generateItem()
+    {
+        UIObject obj =  Instantiate(itemObj,transform);
+        obj.gameObject.SetActive(true);
+        return obj;
+    }
+
+    void setItemView(int i,UIObject obj)
+    {
+        obj.getChild("data1").GetComponent<Text>().text = testDatList[i].data1.ToString();
+        obj.getChild("data2").GetComponent<Text>().text = testDatList[i].data2.ToString();
+        obj.getChild("data3").GetComponent<Text>().text = testDatList[i].data3.ToString();
+    }
+
+}
---
+++ UI/VirtualList.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace BJSYGameCore.UI
+{
+
+
+    public enum LayoutGroupType
+    {
+        Gird,
+        Horizontal,
+        Vertical,
+    }
+    public class VirtualList<T> where T : UIObject
+    {
+        public event Action<int,T> onDisPlayItem;
+        
+        private LayoutGroupType layoutGroupType;
+        private Func<T> itemGernerator;
+        
+        private int frontIndex  = 0;
+        private int backIndex = -1;
+
+        public int HorizontalCount { get; set; }
+        public int VerticalCount { get; set; }
+
+        public int TotalCount { get { return HorizontalCount* VerticalCount; } }
+
+        private List<T> realItems;
+
+        private Vector2 oldScrollRectVal = Vector2.zero;
+
+        public VirtualList(Func<T> itemGernerator, LayoutGroup layoutGroup)
+        {
+            realItems = new List<T>();
+            ScrollRect scrollRect = layoutGroup.transform.GetComponentInParent<ScrollRect>();
+            Rect viewPortRect = scrollRect.GetComponent<RectTransform>().rect;
+            float realHeight = viewPortRect.height - layoutGroup.padding.top - layoutGroup.padding.bottom;
+            float realWidth = viewPortRect.width - layoutGroup.padding.left - layoutGroup.padding.right;
+            if(layoutGroup is GridLayoutGroup)
+            {
+                layoutGroupType = LayoutGroupType.Gird;
+                GridLayoutGroup gridLayoutGroup = layoutGroup as GridLayoutGroup;
+                HorizontalCount = (int)((realWidth + gridLayoutGroup.spacing.x) / (gridLayoutGroup.cellSize.x + gridLayoutGroup.spacing.x));
+                VerticalCount =2+(int)((realHeight + gridLayoutGroup.spacing.y) / (gridLayoutGroup.cellSize.y + gridLayoutGroup.spacing.y));
+            }
+            if(layoutGroup is VerticalLayoutGroup)
+            {
+                layoutGroupType = LayoutGroupType.Vertical;
+                VerticalLayoutGroup verticalLayoutGroup = layoutGroup as VerticalLayoutGroup;
+                HorizontalCount = 1;
+                VerticalCount =2 + (int)((realHeight + verticalLayoutGroup.spacing) /
+                    (verticalLayoutGroup.transform.GetChild(1).GetComponent<RectTransform>().rect.height + verticalLayoutGroup.spacing));
+            }
+            if(layoutGroup is HorizontalLayoutGroup)
+            {
+                layoutGroupType = LayoutGroupType.Horizontal;
+                HorizontalLayoutGroup horizontalLayoutGroup = layoutGroup as HorizontalLayoutGroup;
+                VerticalCount = 1;
+                HorizontalCount = 2 + (int)((realWidth + horizontalLayoutGroup.spacing) /
+                    (horizontalLayoutGroup.transform.GetChild(1).GetComponent<RectTransform>().rect.width + horizontalLayoutGroup.spacing));
+            }
+            Debug.Log($"{HorizontalCount}**{VerticalCount}");
+            
+            scrollRect.onValueChanged.AddListener(onPlayerDrag);
+            this.itemGernerator = itemGernerator;
+        }
+
+        public T addItem()
+        {
+            if (realItems.Count < TotalCount)
+            {
+                var item  = itemGernerator?.Invoke();
+                realItems.Add(item as T);
+                backIndex += 1;
+                onDisPlayItem?.Invoke(backIndex,realItems[backIndex]);
+                return item as T;
+            }
+            return null;
+        }
+
+        void onPlayerDrag(Vector2 val)
+        {
+            if(oldScrollRectVal == Vector2.zero)
+            {
+                oldScrollRectVal = val;
+                return;
+            }
+            else
+            {
+                //列表向下滑动
+                if (oldScrollRectVal.x < val.x)
+                {
+
+                }
+                else // 列表向上滑动
+                {
+
+                }
+            }
+            Vector3 firstItemPos = realItems[0].transform.localPosition;
+            switch (layoutGroupType)
+            {
+                //case LayoutGroupType.Gird:
+                //    if (firstItemPos.x +)
+            }
+
+            Debug.Log($"{firstItemPos.x}TTT{firstItemPos.y}");
+            Debug.Log($"{val.x}|||{val.y}");
+        }
+    }
+
+}
