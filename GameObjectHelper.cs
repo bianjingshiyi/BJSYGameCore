82f01b817d76948e864541aa1d7008eab08f6985
bjsy <bianjingshiyi@hotmail.com>
2022-05-19 10:36:24 +0800

    新增TypeStringDrawer

--- GameObjectHelper.cs
+++ GameObjectHelper.cs
@@ -4,9 +4,16 @@ namespace BJSYGameCore
 {
     public static class GameObjectHelper
     {
-        public static void setGameObjectLayer(GameObject gameObject, LayerMask layerMask)
+        public static void setGameObjectLayer(GameObject gameObject, LayerMask layerMask, bool parentOnly = false)
         {
             gameObject.layer = (int)Mathf.Log(layerMask.value, 2);
+            if (!parentOnly)
+            {
+                for (int i = 0; i < gameObject.transform.childCount; i++)
+                {
+                    setGameObjectLayer(gameObject.transform.GetChild(i).gameObject, layerMask, parentOnly);
+                }
+            }
         }
     }
 }
